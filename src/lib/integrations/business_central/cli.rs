2302b92dec9f361a0ad8715bf0aea54089695889
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-04-28 23:50:36 -0500

    Setup BC direct API query

--- src/lib/integrations/business_central/cli.rs
+++ src/lib/integrations/business_central/cli.rs
@@ -1,3 +1,4 @@
+use business_central::UrlKeyValue;
 use clap::{Arg, ArgMatches, Command};
 use figment::Figment;
 use futures::executor;
@@ -6,6 +7,37 @@ use serde::Deserialize;
 pub(crate) fn build_command() -> Command<'static> {
     Command::new("business-central")
         .about("Interact with Business Central")
+        .subcommand(
+            Command::new("api")
+                .about("Make authenticated HTTP requests")
+                .subcommand(
+                    Command::new("odata")
+                        .about("Make authenticated odata requests")
+                        .arg(
+                            Arg::new("path")
+                                .help("path")
+                                .required(true)
+                                .takes_value(true),
+                        )
+                        .arg(
+                            Arg::new("resource_values")
+                                .help("resource_values")
+                                .multiple_values(true)
+                                .takes_value(true),
+                        ),
+                )
+                .subcommand(
+                    Command::new("unbound")
+                        .about("Make authenticated unbound requests")
+                        .arg(
+                            Arg::new("path")
+                                .help("path")
+                                .required(true)
+                                .multiple_values(true)
+                                .takes_value(true),
+                        ),
+                ),
+        )
         .subcommand(
             Command::new("orders")
                 .about("Interact with orders")
@@ -42,8 +74,35 @@ pub(crate) fn process_matches(config_builder: Figment, matches: &ArgMatches) {
         config.username,
         config.web_service_access_key,
     );
-
-    if let Some(matches) = matches.subcommand_matches("orders") {
+    if let Some(matches) = matches.subcommand_matches("api") {
+        if let Some(matches) = matches.subcommand_matches("odata") {
+            if let Some(path) = matches.value_of("path") {
+                let mut resource_values: Vec<UrlKeyValue> = Vec::new();
+                if matches.is_present("resource_values") {
+                    let values: Vec<&str> = matches.values_of("resource_values").unwrap().collect();
+                    for value in values {
+                        let parsed_value = value.parse::<i16>();
+                        if let Ok(..) = parsed_value {
+                            resource_values.push(UrlKeyValue::Number(parsed_value.unwrap()))
+                        } else {
+                            resource_values.push(UrlKeyValue::Text(value.to_string()))
+                        }
+                    }
+                }
+                let sales_order = executor::block_on(
+                    business_central::resources::sales_orders::handlers::get_generic(
+                        client,
+                        path,
+                        resource_values,
+                    ),
+                )
+                .unwrap();
+                tracing::error!("{:#?}", sales_order)
+            }
+        } else if let Some(_matches) = matches.subcommand_matches("unbound") {
+            tracing::error!("{}", "unimplemented!");
+        }
+    } else if let Some(matches) = matches.subcommand_matches("orders") {
         if let Some(matches) = matches.subcommand_matches("get") {
             if let Some(order_number) = matches.value_of("order-number") {
                 let sales_order = executor::block_on(
