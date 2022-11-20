482ae11d0d08c7b0e2918cbbda6c6bf1a511a05a
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-11-19 18:24:43 -0600

    attach eval to clap
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/integrations/python/cli.rs
+++ src/integrations/python/cli.rs
@@ -1,32 +1,38 @@
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
 use figment::Figment;
-use pyo3::prelude::*;
-use pyo3::types::IntoPyDict;
+use pyo3::{prelude::*, types::PyDict};
 
 use crate::Context;
 
 pub(crate) fn build_command() -> Command {
     Command::new("python")
         .about("Interact with Python")
-        .subcommand(Command::new("sample").about("Run sample"))
+        .subcommand(
+            Command::new("eval")
+                .about("Evaluate code")
+                .arg(Arg::new("code").help("The code to evaluate")),
+        )
 }
 
-pub(crate) fn process_matches(_context: &Context, _config_builder: &Figment, matches: &ArgMatches) {
-    if let Some(_matches) = matches.subcommand_matches("sample") {
-        run_py_sample().unwrap();
+pub(crate) fn process_matches(context: &Context, _config_builder: &Figment, matches: &ArgMatches) {
+    if let Some(matches) = matches.subcommand_matches("eval") {
+        if let Some(code) = matches.get_one::<String>("code") {
+            let string = match run_py(code) {
+                Ok(value) => value,
+                Err(error) => error.to_string(),
+            };
+            if !context.quiet {
+                println!("{:#?}", string);
+            }
+        }
     }
 }
 
-fn run_py_sample() -> PyResult<()> {
+fn run_py(code: &str) -> Result<String, PyErr> {
     Python::with_gil(|py| {
-        let sys = py.import("sys")?;
-        let version: String = sys.getattr("version")?.extract()?;
-
-        let locals = [("os", py.import("os")?)].into_py_dict(py);
-        let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-        let user: String = py.eval(code, None, Some(locals))?.extract()?;
-
-        println!("Hello {}, I'm Python {}", user, version);
-        Ok(())
+        let locals = PyDict::new(py);
+        py.run(code, None, Some(locals))?;
+        let output = locals.get_item("output").unwrap();
+        Ok(output.to_string())
     })
 }
